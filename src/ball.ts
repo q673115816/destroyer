8a4b3fa344ca92c78af3814e35d3121aff28f998
林锦骏 <673115816@qq.com>
2022-09-05 00:02:47 +0800

    save

--- src/ball.ts
+++ src/ball.ts
@@ -1,13 +1,35 @@
 import { WIDTH, HEIGHT, init } from './config'
-const { canvas, ctx } = init('#ball')
 const SIZE = 5
 const balls: Ball[] = []
+
+class BallList {
+  list: Ball[] = []
+  private timer: number | null = null
+  private canvas: HTMLCanvasElement
+  private ctx: CanvasRenderingContext2D
+  constructor() {
+    const { canvas, ctx } = init('#ball')
+    this.canvas = canvas
+    this.ctx = ctx
+    this.draw()
+  }
+
+  draw() {
+    this.ctx.clearRect(0, 0, WIDTH, HEIGHT)
+    this.ctx.beginPath()
+    this.ctx.fillStyle = '#fff'
+    this.list.forEach(({ x, y }) => {
+      this.ctx.arc(x, y, SIZE, 0, 2 * Math.PI)
+    })
+    this.ctx.fill()
+    this.ctx.restore()
+    this.timer = requestAnimationFrame(this.draw)
+  }
+}
 
 export default class Ball {
   x: number
   y: number
-  private timer: number | null = null
-  private state: boolean = false
   private size = SIZE
   private xv: number = Math.random() * 2 - 1
   private yv: number = Math.random() * 2 - 1
@@ -18,11 +40,6 @@ export default class Ball {
     this.start()
   }
 
-  init() {
-    this.register()
-    this.draw()
-  }
-
   start() {
     this.x += this.xv
     this.y += this.yv
@@ -31,24 +48,5 @@ export default class Ball {
     if (this.x < 0) this.x = -this.x
     if (this.y < 0) this.y = -this.y
     if (this.x - this.size >= WIDTH) this.x -= WIDTH - this.x + this.size
-  }
-
-  register() {
-    document.addEventListener('keydown', (event) => {
-      const { code } = event
-      if (code === 'Space') this.state = true
-    })
-  }
-
-  draw() {
-    ctx.clearRect(0, 0, WIDTH, HEIGHT)
-    ctx.beginPath()
-    ctx.fillStyle = '#fff'
-    balls.forEach(({ x, y }) => {
-      ctx.arc(x, y, SIZE, 0, 2 * Math.PI)
-    })
-    ctx.fill()
-    ctx.restore()
-    this.timer = requestAnimationFrame(this.draw)
   }
 }
