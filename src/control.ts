133dc9dc18919a2d911c6d3a5a11c2bf35a7126c
林锦骏 <673115816@qq.com>
2022-09-06 11:43:12 +0800

    save

--- src/control.ts
+++ src/control.ts
@@ -1,4 +1,5 @@
 import { clamp } from "lodash";
+import { concatAll, fromEvent, takeUntil, map } from "rxjs";
 import { Common, WIDTH, HEIGHT } from "./common";
 type Code = "ArrowLeft" | "ArrowRight";
 
@@ -10,7 +11,7 @@ export default class Control extends Common {
     ArrowRight: false,
   };
   x = WIDTH / 2 - this.width / 2;
-  y = HEIGHT - 100
+  y = HEIGHT - 200;
   v = 5;
   constructor() {
     super("#control");
@@ -19,6 +20,8 @@ export default class Control extends Common {
 
   init() {
     this.register();
+    this.registerMause();
+    this.registerTouch();
   }
 
   update() {
@@ -42,5 +45,75 @@ export default class Control extends Common {
       const { code } = event;
       if (code in this.state) this.state[code as Code] = false;
     });
+  }
+
+  registerMause() {
+    const mouseDown$ = fromEvent<MouseEvent>(
+      document.querySelector("#control") as HTMLCanvasElement,
+      "mousedown"
+    );
+    const mouseMove$ = fromEvent<MouseEvent>(document, "mousemove");
+    const mouseUp$ = fromEvent<MouseEvent>(document, "mouseup");
+
+    mouseDown$
+      .pipe(
+        // filter((e) => {
+        //   return e.target === el;
+        // }),
+        map((e) => {
+          return {
+            x: this.x,
+            startX: e.clientX,
+          };
+        }),
+        map(({ x, startX }) =>
+          mouseMove$.pipe(
+            takeUntil(mouseUp$),
+            map(({ clientX }) => ({
+              x,
+              left: clientX - startX,
+            }))
+          )
+        ),
+        concatAll()
+      )
+      .subscribe({
+        next: (data) => {
+          this.x = data.x + data.left;
+        },
+      });
+  }
+
+  registerTouch() {
+    const touchStart$ = fromEvent<TouchEvent>(
+      document.querySelector("#control") as HTMLCanvasElement,
+      "touchstart"
+    );
+    const touchMove$ = fromEvent<TouchEvent>(document, "touchmove");
+    const touchEnd$ = fromEvent<TouchEvent>(document, "touchend");
+    touchStart$
+      .pipe(
+        map(({ changedTouches }) => {
+          return {
+            x: this.x,
+            startX: changedTouches[0].clientX,
+          };
+        }),
+        map(({ x, startX }) =>
+          touchMove$.pipe(
+            takeUntil(touchEnd$),
+            map(({ changedTouches }) => ({
+              x,
+              left: changedTouches[0].clientX - startX,
+            }))
+          )
+        ),
+        concatAll()
+      )
+      .subscribe({
+        next: (data) => {
+          this.x = data.x + data.left;
+        },
+      });
   }
 }
